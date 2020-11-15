c5644edc471d30dd11a3f5c671d9a2631d415b82
netscout <netscout82@naver.com>
2020-11-15 11:16:33 +0900

    C#9.0 소개 추가

---
+++ CSharp9/CSharp9/Program.cs
@@ -0,0 +1,20 @@
+﻿record Hero(string Name, int Age);
+
+static class HeroExt
+{
+    public static string IsHero(this Hero h) => h.Name switch
+    {
+        "임꺽정" or "홍길동" => "한국 영웅",
+        "아이언맨" or "캡틴" => "미국 영웅",
+        _ => "영웅 아님"
+    };
+
+    public static string GetPower(this Hero h) => h.Age switch
+    {
+        < 15 => "약함",
+        >= 16 and <= 29 => "강함",
+        >= 30 and <= 49 => "아직은 강함",
+        >= 50 and <= 69 => "무리 하지 말자",
+        _ => "조용히 있자"
+    };
+}
---
+++ CSharp9/CSharp9/Program2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.WriteLine("Hello World!");
+
+var hero1 = new Hero("홍길동", 29);
+
+var hero2 = new Hero("아이언맨", 49);
+
+Console.WriteLine(hero1.IsHero());
+Console.WriteLine(hero2.IsHero());
+Console.WriteLine(hero1.GetPower());
+Console.WriteLine(hero2.GetPower());
