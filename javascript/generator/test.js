a74569e61866f065c0c58f96307d355fdf8bf78d
netscout <netscout82@naver.com>
2020-11-22 11:09:31 +0900

    js generator 추가

--- javascript/generator/test.js
+++ javascript/generator/test.js
@@ -1,8 +1,44 @@
-let items = []
+function range(start = 0, end = Infinity, step = 1) {
+    let result = []
 
-for(let i=0; i < 100; i++) {
-    items.push(i)
+    for(let i = start; i < end; i += step) {
+        result.push(i);
+    }
+
+    return result;
 }
 
-console.log(items)
+function *range2(start = 0, end = Infinity, step = 1) {
+    for(let i = start; i < end; i += step) {
+        yield i;
+    }
+}
 
+for(num of range(5, 10)) {
+    console.log(num);
+}
+
+function checkMemory(func) {
+    const beforeKB = memoryUsedKB();
+    console.log("memory before: ", beforeKB);
+
+    let sum = 0;
+    for(let i of func(0, 10000)) {
+        sum += i;
+    }
+
+    console.log("sum: ", sum);
+
+    const afterKB = memoryUsedKB();
+    console.log("memory after: ", afterKB);
+
+    const diffKB = afterKB - beforeKB;
+    console.log("memory diff: ", diffKB);
+}
+
+function memoryUsedKB() {
+    return Math.round(process.memoryUsage().heapTotal * 100 / 1024) / 100;
+}
+
+checkMemory(range);
+checkMemory(range2);
